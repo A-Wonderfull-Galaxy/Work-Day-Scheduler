9f8538f7c53315501a79778e736df3798242e77b
A-Wonderfull-Galaxy <marlonq16@gmail.com>
2022-04-02 21:05:45 -0400

    fixed current date el

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
 $(document).ready(function(){
 
     //display current date
-    let currentDateEl = moment().format("dd MM D, YYYY");
-    $('currentDay').text(currentDateEl);
+    let currentDateEl = moment().format("dddd MMMM D, YYYY");
+    $('#currentDay').text(currentDateEl);
 
     //time blocks change color based on hour of day
     let hourEl= moment().get("hour");
@@ -19,5 +19,19 @@ $(document).ready(function(){
             $(this).addClass("future");
         }
     });
+    
+    //variable to declare all time block
+    let eventsEl = document.querySelectorAll('.event');
 
+    //forEach loop to save event input
+    eventsEl.forEach(function(event,i){
+        const eventInput = event.querySelector('textarea');
+        const saveBtn = event.querySelector('button');
+        eventInput.value = localStorage.getItem('event-' + (i+1))
+
+        //on click save, event listner
+        saveBtn.addEventListener('click', function(){
+            localStorage.setItem('event-' + (i+1), eventInput.value);
+        })
+    })
 });
